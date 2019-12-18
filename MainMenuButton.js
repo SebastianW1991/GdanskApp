c91d5f91f730adfd34158affb970c47c07717cf3
SebastianW1991 <sebastianw1991@gmail.com>
2019-12-18 20:30:12 +0100

    Create MainMenuButton.js

---
+++ MainMenuButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, Text, StyleSheet,ImageBackground } from 'react-native';
+
+class MainMenuButton extends Component {
+	render() {
+		const { text, onPress} = this.props;
+		return (
+          <TouchableOpacity style={styles.buttonStyle}
+          
+			onPress={() => onPress()}
+		  >
+
+			 <Text style={styles.textStyle}>{text}</Text>
+
+		  </TouchableOpacity>
+		);
+	}
+}
+
+MainMenuButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize:20,
+	color: '#ffffff',
+	textAlign: 'center'
+  },
+  
+  buttonStyle: {
+	padding:10,
+	backgroundColor: '#202646',
+	borderRadius:5
+  }
+});
+
+export default MainMenuButton;
