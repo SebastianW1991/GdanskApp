1183a6f20af7baf3ca608f6ad82a6b8a744451da
SebastianW1991 <sebastianw1991@gmail.com>
2019-12-26 15:12:23 +0100

    Update GdanskNavigator.js

--- navigation/GdanskNavigator.js
+++ navigation/GdanskNavigator.js
@@ -2,13 +2,13 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
 import HistoryScreen from '../screens/HistoryScreen';
-import MiddleageScreen from '../screens/MiddleageScreen';
+import MedievalScreen from '../screens/MedievalScreen';
 
 
 const GdanskNavigator = createStackNavigator({
   Home: HomeScreen,
   History: HistoryScreen,
-  MiddleAge:MiddleageScreen
+  Medieval:MedievalScreen
 });
 
 export default createAppContainer(GdanskNavigator);
