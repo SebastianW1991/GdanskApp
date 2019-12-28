490106a5b6d9cb513907afb1284f15123b4c5dca
SebastianW1991 <sebastianw1991@gmail.com>
2019-12-28 19:51:19 +0100

    Create TourDolneMiastoScreen.js

---
+++ screens/TourDolneMiastoScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, ScrollView,Image,TouchableOpacity, ImageBackground } from 'react-native';
+
+
+const TourDolneMiastoScreen = props => {
+    return (
+        <ScrollView>
+            <Text>Gdańsk w Średniowieczu</Text>
+            <Text>Średniowiecze jest dla Gdańska okresem szczególny.
+                 Wtedy to powstaje  znany z czasów dzisiejszych obrys Śródmieścia. Czyli Stare Miasto oraz Główne Miasto, 
+                 wtedy też powstaje wiekszość z gdańskich budowli sakralnych takich jak</Text>
+
+        </ScrollView>
+
+    )
+};
+export default TourDolneMiastoScreen;
