86cc718b87af1ef15c7b58176433a85f6d6e845f
SebastianW1991 <sebastianw1991@gmail.com>
2020-01-17 20:48:47 +0100

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,58 +1,30 @@
 import React from 'react';
-import { View,  StyleSheet, Button, Text, Image, TouchableOpacity } from 'react-native';
+import { View,  StyleSheet, Button, Text, Image, TouchableOpacity, ImageBackground } from 'react-native';
 import MainMenuButton from '../MainMenuButton';
 
 const HomeScreen = props => {
   return (
-
-      <View style={styles.MainScreen}>
-        <View style={styles.LogoSection}>
-          <Text>Tu będzie logo</Text>
-        </View>
-       
-        <View style={styles.MenuSection}>
-        <MainMenuButton 
+    <ImageBackground source={require('./assets/MainPhoto.jpg')} style={{width: '100%', height: '100%'}}>
+      <View style={styles.MenuSection}>
+          <MainMenuButton 
 		      text="Historia"
           onPress={() => {
           props.navigation.navigate({
           routeName: 'History'
-        })
-      }}		
-		/>
-         <MainMenuButton 
-		      text="Historia"
+          })
+          }}		
+		      />
+          <MainMenuButton 
+		      text="Wycieczki"
           onPress={() => {
           props.navigation.navigate({
-          routeName: 'History'
-        })
-      }}		
-		/>
-        <MainMenuButton 
-		      text="Historia"
-          onPress={() => {
-          props.navigation.navigate({
-          routeName: 'History'
-        })
-      }}		
-		/>
-        <MainMenuButton 
-		      text="Historia"
-          onPress={() => {
-          props.navigation.navigate({
-          routeName: 'History'
-        })
-      }}		
-		/>
+          routeName: 'Tours'
+          })
+          }}		
+		      />
+      </View>    
+  </ImageBackground>
 
-        </View>
-        
-
-           
-   
-
-
-
- </View>
   )
 
 };
@@ -63,20 +35,16 @@ MainScreen: {
     backgroundColor: '#FEED24',
     flex: 1,
     justifyContent: 'flex-start',
-    width:'100%'
-    
-  },
-  LogoSection:{
-    flex:0.2,
-    backgroundColor:'white'
-
+    width:'100%',
+    backgroundColor:'red',
+    opacity: 0.2,
   },
   MenuSection:{
-    backgroundColor:'red',
     flex:1,
     alignSelf:'center',
-    width:'80%',
-    flexDirection:"column"
+    width:'100%',
+    flexDirection:"column",
+    alignContent:"center",
   },
 
 
