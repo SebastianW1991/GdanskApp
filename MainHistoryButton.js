50990d4b1b3609da81c5a2a4c629b21c493b6b4e
SebastianW1991 <sebastianw1991@gmail.com>
2020-01-23 18:12:53 +0100

    Create MainHistoryButton.js

---
+++ MainHistoryButton.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+class MainHistoryButton extends Component {
+	render() {
+		const { text, onPress} = this.props;
+		return (
+          <TouchableOpacity style={styles.buttonStyle}
+          
+			onPress={() => onPress()}
+		  >
+
+			 <Text style={styles.textStyle}>{text}</Text>
+
+		  </TouchableOpacity>
+		);
+	}
+}
+
+MainHistoryButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize:24,
+	color: 'black',
+	textAlign: 'center',
+    opacity:1,
+    fontFamily:'Chomsky'
+  },
+  
+  buttonStyle: {
+	padding:10,
+	backgroundColor: 'red',
+	borderRadius:25,
+	width:'80%',
+	alignSelf:"center",
+	opacity:0.6,
+	marginTop:10
+  }
+});
+
+export default MainHistoryButton;
