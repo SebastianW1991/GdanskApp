43cdf76fc7774f44b383e19d67423c39d1786cf4
SebastianW1991 <sebastianw1991@gmail.com>
2020-01-29 21:33:49 +0100

    Update GdanskNavigator.js
    Updated navigation

--- navigation/GdanskNavigator.js
+++ navigation/GdanskNavigator.js
@@ -1,36 +1,54 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import HomeScreen from '../screens/HomeScreen';
-import HistoryScreen from '../screens/HistoryScreen';
-import MedievalScreen from '../screens/MedievalScreen';
-import ModernScreen from '../screens/ModernScreen';
-import ContemporaryScreen from '../screens/ContemporaryScreen';
-import TourDolneMiastoScreen from '../screens/TourDolneMiastoScreen';
-import TourOliwaScreen from '../screens/TourOliwaScreen';
-import ToursScreen from '../screens/ToursScreen';
-import AboutScreen from '../screens/AboutScreen';
-import EverydayLifeScreen from '../screens/EverydayLifeScreen';
-import VIPScreen from '../screens/VIPScreen';
+import HomeScreen from "../screens/HomeScreen";
+import HistoryScreen from "../screens/HistoryScreen";
+import MedievalScreen from "../screens/MedievalScreen";
+import ModernScreen from "../screens/ModernScreen";
+import ContemporaryScreen from "../screens/ContemporaryScreen";
+import TourDolneMiastoScreen from "../screens/TourDolneMiastoScreen";
+import TourOliwaScreen from "../screens/TourOliwaScreen";
+import ToursScreen from "../screens/ToursScreen";
+import AboutScreen from "../screens/AboutScreen";
+import EverydayLifeScreen from "../screens/EverydayLifeScreen";
+import VIPScreen from "../screens/VIPScreen";
+import JanHeveliusScreen from "../screens/JanHeveliusScreen";
+import DanielGralathScreen from "../screens/DanielGralathScreen";
 
 const GdanskNavigator = createStackNavigator({
   Home: {
-    screen:HomeScreen, navigationOptions: {
-      header: null,
+    screen: HomeScreen,
+    navigationOptions: {
+      header: null
     }
   },
   History: HistoryScreen,
   Medieval: MedievalScreen,
   Contemporary: ContemporaryScreen,
   Modern: ModernScreen,
-  DolneMiasto:TourDolneMiastoScreen,
-  Oliwa:TourOliwaScreen,
-  Tours:ToursScreen,
-  About:AboutScreen,
-  EverydayLife:EverydayLifeScreen,
-  VIP:VIPScreen,
-
-
-
+  DolneMiasto: TourDolneMiastoScreen,
+  Oliwa: TourOliwaScreen,
+  Tours: ToursScreen,
+  About: AboutScreen,
+  EverydayLife: EverydayLifeScreen,
+  VIP: VIPScreen,
+  Hevelius: {
+    screen: JanHeveliusScreen,
+    navigationOptions: {
+      title: "Jan Heweliusz",
+      headerStyle: {
+        fontSize: 24
+      }
+    }
+  },
+  Gralath: {
+    screen: DanielGralathScreen,
+    navigationOptions: {
+      title: "Daniel Gralath",
+      headerStyle: {
+        fontSize: 24
+      }
+    }
+  }
 });
 
 export default createAppContainer(GdanskNavigator);
